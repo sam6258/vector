70dbb5537ff95594cd9fca7d1eba324820127f1b
Scott Miller <smiller1@coreweave.com>
2024-11-18 02:54:11 -0500

    fix(aws_s3): remove force_path_style from aws_s3 client

--- src/common/s3.rs
+++ src/common/s3.rs
@@ -8,7 +8,7 @@ impl ClientBuilder for S3ClientBuilder {
     type Client = aws_sdk_s3::client::Client;
 
     fn build(config: &aws_types::SdkConfig) -> Self::Client {
-        let config = config::Builder::from(config).force_path_style(true).build();
+        let config = config::Builder::from(config).build();
         aws_sdk_s3::client::Client::from_conf(config)
     }
 }
